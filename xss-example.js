f360dc2ec1a865b7c20fe2a26dff5465632b10da
Anatoli <155424115+anatolimobb@users.noreply.github.com>
2024-01-07 16:44:24 +0200

    Update xss-example.js

--- xss-example.js
+++ xss-example.js
@@ -1,5 +1,6 @@
 // This is an intentionaly vulnerable Javascript file used for testing, do not use or deploy it anywhere.
 
-var name = urlParams.get("name");
-var unsafe_div = window.document.getElementById("vulnerable-div");
-unsafe_div.innerHTML = "Hello " + name;
+var urlParams = new URLSearchParams(window.location.search);
+var username = urlParams.get("username");
+
+document.write("Hello to y111ou!!!!!!!, " + username + "!!!");
